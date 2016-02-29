961a5fc8d53ac423cbaa3e7f0d50ddd1439b1bf5
AnTxXx <urbanek.michael@gmx.at>
2016-02-29 15:49:15 +0100

    Added RGBConverter and implemented pulsating.
    If it does not recognize RGBConverter, copy the folder from the libs folder to your OS private documents arduino folder.

--- discoobe200/discoobe200.ino
+++ discoobe200/discoobe200.ino
@@ -1,5 +1,6 @@
-
+#include <RGBConverter.h>
 #include <Adafruit_NeoPixel.h>
+
 #include <stdio.h>
 
 //............................................NrOfPixel, Pin, Std.
@@ -9,6 +10,11 @@ Adafruit_NeoPixel areaEast   = Adafruit_NeoPixel(9, 12, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel areaSouth   = Adafruit_NeoPixel(9, 11, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel areaWest   = Adafruit_NeoPixel(9, 10, NEO_GRB + NEO_KHZ800);
 
+RGBConverter colorConv;
+
+int r,g,b;
+double v = 0.0; //Helligkeit
+double vSteps = 0.05; //Helligskeitsschritte
 
 long previousMillis = 0;
 long onTime = 250;
@@ -32,8 +38,6 @@ int r3 = rand() % 255;
 int g3 = rand() % 255;
 int b3 = rand() % 255;
 
-
-
 // the setup function runs once when you press reset or power the board
 void setup() {
   areaTop.begin();
@@ -41,44 +45,88 @@ void setup() {
   areaEast.begin();
   areaSouth.begin();
   areaWest.begin();
+
+  r = 255;
+  g = 0;
+  b = 0;
   Serial.begin(9600);
   //while (!Serial) {
   //  ; // wait for serial port to connect. Needed for native USB port only
   //}
   Serial.println("Discoobe is up and running");
-
+  
   //
 }
 
+unsigned long lastChange = 0.0;
+bool pulseOn = false;
+int pulseColorR = 0, pulseColorG = 0, pulseColorB = 0;
 // the loop function runs over and over again forever
+
 void loop() {
-
-  Serial.println("blink on");
-  r0 = rand() % 255;
-  g0 = rand() % 255;
-  b0 = rand() % 255;
-
-  int r = rand() % 10;
-  if (r < 5) {    
-      int row = rand() % 3;
-      paintRow (row, r0, g0, b0);
+  int randi = rand() % 40;
+  
+  if(randi >37 && !pulseOn){
+    pulseOn = true;
+    lastChange = millis();
+    pulseColorR = rand() % 255;
+    pulseColorG = rand() % 255;
+    pulseColorB = rand() % 255;
   }
 
-  if (r == 5)
-    paintFace ('T', r0, g0, b0);
-  if (r == 6)
-    paintFace ('N', r0, g0, b0);
-  if (r == 7)
-    paintFace ('S', r0, g0, b0);
-  if (r == 8)
-    paintFace ('W', r0, g0, b0);            
-  if (r == 9)
-    paintFace ('E', r0, g0, b0);                
+  // 12 Sekunden pulsieren
+  if(lastChange + 12000 < millis()){
+    pulseOn = false;
+  }
 
-  setCubeColors ();
+  // Beim Pulsieren muss die Farbe immer wieder gesetzt werden, sonst verändert sie sich
+  if(pulseOn){
+    r = pulseColorR;
+    g = pulseColorG;
+    b = pulseColorB;
+    v = (v + vSteps);
+    if(v > 0.95 || v < 0.05){
+      vSteps = vSteps * (-1.0);
+    }
+
+    pulseColor(r, g, b, v);
+    paintFace ('T', r, g, b);
+    paintFace ('N', r, g, b);
+    paintFace ('S', r, g, b);
+    paintFace ('W', r, g, b);            
+    paintFace ('E', r, g, b);    
+    
+    setCubeColors ();
+    waitMilliSeconds = 100;  
+    delay (waitMilliSeconds);
+  } else {
+    // Andere Muster außer Pulsieren
+    r0 = rand() % 255;
+    g0 = rand() % 255;
+    b0 = rand() % 255;
   
-  waitMilliSeconds = 450 + rand() % 455;  
-  delay (waitMilliSeconds);
+    if (randi < 20) {    
+        int row = rand() % 3;
+        paintRow (row, r0, g0, b0);
+    }
+  
+    if (randi>20)
+      paintFace ('T', r0, g0, b0); else
+    if (randi >23)
+      paintFace ('N', r0, g0, b0); else
+    if (randi >26)
+      paintFace ('S', r0, g0, b0); else
+    if (randi >29)
+      paintFace ('W', r0, g0, b0); else   
+    if (randi >32)
+      paintFace ('E', r0, g0, b0);           
+  
+    
+    setCubeColors ();
+    
+    waitMilliSeconds = 200 + rand() % 380;
+    delay (waitMilliSeconds);
+  }
 }
 
 //
@@ -135,6 +183,27 @@ void setCubeColors ()
     areaEast.show();   
     areaSouth.show();   
     areaWest.show();   
+}
+
+//
+//
+//
+void pulseColor (int &r, int &g, int &b, double v) // pulsateAColor
+{
+  double hsv[3];
+  byte rgb[3];
+  
+  byte rB = r;
+  byte gB = g;
+  byte bB = b;
+  
+  colorConv.rgbToHsv(rB, gB, bB, hsv);
+  //Serial.print(hsv[2]);
+  colorConv.hsvToRgb(hsv[0], hsv[1], v, rgb);
+
+  r = rgb[0];
+  g = rgb[1];
+  b = rgb[2];
 }
 
 //
